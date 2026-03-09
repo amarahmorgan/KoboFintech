3261b0fdaaf01558e198f582505e7a48abf649ec
Amarah Morgan <amarahmorgan@gmail.com>
2026-03-09 08:29:56 +0200

    Updated queries

--- Sql-Queries/revenue-analysis.sql
+++ Sql-Queries/revenue-analysis.sql
@@ -1,12 +1,12 @@
 
-SELECT 
+SELECT
     u.AccountTier,
     COUNT(tl.EntryID) AS TotalTransactions,
     SUM(tl.Amount) AS TotalRevenue
 FROM TransactionLedger tl
-JOIN Wallets w 
+    JOIN Wallets w
     ON tl.WalletID = w.WalletID
-JOIN Users u
+    JOIN Users u
     ON w.UserID = u.UserID
 WHERE tl.ProcessingStatus = 'Completed'
 GROUP BY u.AccountTier
